011f82dad8ebfebb06ced0c2adab791ef97c9599
Rusl4na <the1234581@gmail.com>
2023-11-28 06:31:05 +0100

    A commit to test branch

--- t.cpp
+++ t.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
 	cout << "Main branch prog" << endl;
-	cout << "The second line" <<endl;
+	cout << "The second line" << endl;
+	cout << "Test branch" << endl;
 	return 0;
 }
 
